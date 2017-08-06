d46f9581e362ab1786c8cb5031eea3eb608a8156
vagrant <CarolynWebster@users.noreply.github.com>
2017-08-06 22:27:25 +0000

    SQLAlchemy Practice Assessment

---
+++ model.py
@@ -0,0 +1,105 @@
+"""Models and database functions for cars db."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# Here's where we create the idea of our database. We're getting this through
+# the Flask-SQLAlchemy library. On db, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Part 1: Compose ORM
+
+class Brand(db.Model):
+    """Car brand."""
+
+    __tablename__ = "brands"
+
+    brand_id = db.Column(db.String(5), primary_key=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    founded = db.Column(db.Integer)
+    headquarters = db.Column(db.String(50))
+    discontinued = db.Column(db.Integer)
+
+    #establish relationship with Model table
+    models = db.relationship('Model', backref='brand')
+
+    #establish relationship with Awards table
+    awards = db.relationship('Award', backref='brand')
+
+
+    def __repr__(self):
+        return "<Brand id={} name={}>".format(self.brand_id, self.name)
+
+
+class Model(db.Model):
+    """Car model."""
+
+    __tablename__ = "models"
+
+    model_id = db.Column(db.Integer, primary_key=True,
+                                     autoincrement=True,
+                                     nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    brand_id = db.Column(db.String(5), db.ForeignKey('brands.brand_id'),
+                         nullable=False)
+    name = db.Column(db.String(50), nullable = False)
+
+    def __repr__(self):
+        return "<Model brand_id={} name={}>".format(self.brand_id, self.name)
+
+
+# I know this wasn't needed, but figured it was good practice to make awards as well
+class Award(db.Model):
+    """Award model."""
+
+    __tablename__ = "awards"
+
+    award_id = db.Column(db.Integer, primary_key=True,
+                                     autoincrement=True,
+                                     nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    winner_id = db.Column(db.Integer, db.ForeignKey('brands.brand_id'))
+    name = db.Column(db.String(50), nullable = False)
+
+    def __repr__(self):
+        return "<Award year={} name={}>".format(self.year, self.name)
+# End Part 1
+
+
+##############################################################################
+# Helper functions
+
+def init_app():
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our database.
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///cars'
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print "Connected to DB."
